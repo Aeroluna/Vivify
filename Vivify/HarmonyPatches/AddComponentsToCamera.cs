68d2193385e81c48d0fa8ec8ae651017544d3091
Aeroluna <40481393+Aeroluna@users.noreply.github.com>
2023-11-12 00:27:01 -0700

    fix cam2 hopefully

--- Vivify/HarmonyPatches/AddComponentsToCamera.cs
+++ Vivify/HarmonyPatches/AddComponentsToCamera.cs
@@ -10,39 +10,22 @@ namespace Vivify.HarmonyPatches
     [HarmonyPatch(typeof(MainEffectController))]
     internal static class AddComponentsToCamera
     {
-        private static void YeetComponent<T>(GameObject gameObject)
-            where T : Component
-        {
-            T[] existing = gameObject.GetComponents<T>();
-            foreach (T postProcessingController in existing)
-            {
-                Object.Destroy(postProcessingController);
-            }
-        }
-
         private static void SafeAddComponent<T>(GameObject gameObject)
             where T : Component
         {
-            YeetComponent<T>(gameObject);
-            gameObject.AddComponent<T>();
+            if (gameObject.GetComponent<T>() == null)
+            {
+                gameObject.AddComponent<T>();
+            }
         }
 
         [HarmonyPostfix]
-        [HarmonyPatch("OnEnable")]
+        [HarmonyPatch(nameof(MainEffectController.LazySetupImageEffectController))]
         private static void AddComponents(MainEffectController __instance)
         {
             GameObject gameObject = __instance.gameObject;
             SafeAddComponent<PostProcessingController>(gameObject);
             SafeAddComponent<CameraPropertyController>(gameObject);
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch("OnDisable")]
-        private static void Yeet(MainEffectController __instance)
-        {
-            GameObject gameObject = __instance.gameObject;
-            YeetComponent<PostProcessingController>(gameObject);
-            YeetComponent<CameraPropertyController>(gameObject);
         }
     }
 }
