6cea9c24bb3384f862c9b40b011cb8fdbf87e321
Aeroluna <40481393+Aeroluna@users.noreply.github.com>
2024-11-08 00:03:09 -0700

    move strange quad that has shadowcaster for some reason

---
+++ Vivify/HarmonyPatches/MoveAlwaysVisibleQuad.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Heck;
+using UnityEngine;
+
+namespace Vivify.HarmonyPatches;
+
+[HeckPatch(PatchType.Features)]
+internal static class MoveAlwaysVisibleQuad
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(AlwaysVisibleQuad), nameof(AlwaysVisibleQuad.OnEnable))]
+    private static void MoveIt(AlwaysVisibleQuad __instance)
+    {
+        __instance.transform.position = new Vector3(0, -1000, 0);
+    }
+}
